2343ff374f7ee73b00ce273573ad4a3f9bba6310
Dustin Winkler <dustywinkler@gmail.com>
2020-06-17 00:04:07 -0700

    allowed strings of operations, added keyboard support, fixed bugs

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+
+//same operator multiple times broken
+//ie 1+2+3+4+5+6+1, then equals at the end 
+//adds 6 instead of one for the last operation
+
+
 let numbers = document.querySelectorAll('.numbers')
 let inputText = document.querySelector('#input')
 let operators = document.querySelectorAll('.oper')
@@ -7,24 +14,99 @@ let decimal = document.querySelector('#decimal')
 let backspace = document.querySelector('#backspace')
 let operation, total = ''
 let inputvalue, historyvalue
-let inc = 0
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        inputText.innerHTML += number.id
-    })
+let inc, inc2 = 0
+const buttons = Array.from(document.querySelectorAll('.numbers'))
+const buttonCodes = buttons.map(e => {
+    return e.textContent
 })
-operators.forEach((operator) => {
-    operator.addEventListener('click', (e) => {
+const operatorsArray = ['+','-','*','/']
+document.addEventListener('keydown', (e) => {
+    if (buttonCodes.includes(e.key)) {addnum(e)}
+    else if (operatorsArray.includes(e.key)) {operator2(e.key)}
+    else if (e.key === 'Backspace') {    inputText.innerHTML = inputText.innerHTML.slice(0, inputText.innerHTML.length - 1)}
+    else if (e.key === 'Enter' || e.key === '=') {if (inc == 0) {inputvalue = inputText.innerHTML}
+    dothemath(operation, historyvalue, inputvalue)
+    inc += 1
+    inc2 = 0}
+    else if (e.key === 'c') {inputText.innerHTML = ''
+    history.innerHTML = ''
+    inc = 0
+    inc2 = 0
+    historyvalue = 0
+    operation = ''}
+})
+
+numbers.forEach(number => {
+    number.addEventListener('click', addnum2)})  
+
+function addnum(meme) {
+    if (inputText.innerHTML.length < 17) {
+        if(buttonCodes.includes(meme.key)){
+        inputText.innerHTML += meme.key}
+    }
+} 
+function addnum2(e) {
+    if (inputText.innerHTML.length < 17) {
+        inputText.innerHTML += e.target.innerHTML
+    }
+}   
+//fix double operator press issue and allow strings of 
+//operations (like kims calculator)
+operators.forEach((meme) => {
+    meme.addEventListener('click', operator)})
+        
+function operator(e) {
+    if (inputText.innerHTML != '') {    
+    if (inc2 < 1) {
         historyvalue = inputText.innerHTML
-        history.innerHTML = inputText.innerHTML + ' ' + operator.id;
+        history.innerHTML = inputText.innerHTML + ' ' + e.target.id;
+        inc = 0
         inputText.innerHTML = ''
-        operation = operator.id
-    })
-})
+        operation = e.target.id
+        inc2 += 1
+        }     
+        else {
+            inputvalue = inputText.innerHTML
+            operation = history.innerHTML.split('').pop()
+            historyvalue = history.innerHTML.slice(0, -2)
+            dothemath(operation, historyvalue, inputvalue)
+            history.innerHTML = inputText.innerHTML + ' ' + operation
+            inputText.innerHTML = ''
+           
+            
+        }
+    }    
+}
+function operator2(key) {   
+    if (inputText.innerHTML != '') {
+    if (inc2 < 1) {
+        historyvalue = inputText.innerHTML
+        history.innerHTML = inputText.innerHTML + ' ' + key;
+        inc = 0
+        inputText.innerHTML = ''
+        operation = key
+        inc2 += 1
+        }     
+        else {
+            inputvalue = inputText.innerHTML
+            operation = history.innerHTML.split('').pop()
+            historyvalue = history.innerHTML.slice(0, -2)
+            dothemath(operation, historyvalue, inputvalue)
+            history.innerHTML = inputText.innerHTML + ' ' + operation
+            inputText.innerHTML = ''
+            
+        
+    
+        }
+    }    
+}
 document.querySelector('#clear').addEventListener('click', (e) => {
     inputText.innerHTML = ''
     history.innerHTML = ''
     inc = 0
+    inc2 = 0
+    historyvalue = 0
+    operation = ''
 })
 backspace.addEventListener('click', (e) => {
     inputText.innerHTML = inputText.innerHTML.slice(0, inputText.innerHTML.length - 1)
@@ -33,14 +115,13 @@ decimal.addEventListener('click', (e) => {
     if (!inputText.innerHTML.includes('.')) {inputText.innerHTML += '.'}
 })
 equals.addEventListener('click', (e) => {
-    if (inc == 0) {inputvalue = inputText.innerHTML}
+    if (inc == 0) {inputvalue = inputText.innerHTML} 
     dothemath(operation, historyvalue, inputvalue)
     inc += 1
+    inc2 = 0
 })
-function addNum(num) {
-    inputText.innerHTML += num
-}
 function dothemath(operation, a, b) {
+    if (inputText.innerHTML != '') {
     if (operation == '+') {total = parseFloat(a) + parseFloat(b)}
     else if (operation == '/') {total = parseFloat(a) / parseFloat(b)}
     else if (operation == '*') {total = parseFloat(a) * parseFloat(b)}
@@ -48,4 +129,5 @@ function dothemath(operation, a, b) {
     inputText.innerHTML = total
     history.innerHTML = ''
     historyvalue = total
+    }
 }
